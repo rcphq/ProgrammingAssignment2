477af61b4af01571462bd6973ff09f8e51dfa5cf
rcphq <ruben.llibre@gmail.com>
2015-03-18 22:59:48 -0400

    functionaly done
    working, needs comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creating a set of functions to create a cache-friendly matrix
+##  the matrix will be XXXXXXXXXXXXXXXXXXXXXX
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() {x}
+  setinverse <- function(inv)  { m <<- inv}
+  getinverse <- function() {m}
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+    m <- x$getinverse()
+    if (!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
         ## Return a matrix that is the inverse of 'x'
 }
