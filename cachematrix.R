3ee859ba9f85c2239224ffcca03cde7d7a0939ab
rcphq <ruben.llibre@gmail.com>
2015-03-18 23:07:26 -0400

    Including descriptional comments
    comments added, ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,16 @@
 ## Creating a set of functions to create a cache-friendly matrix
-##  the matrix will be XXXXXXXXXXXXXXXXXXXXXX
+##  the matrix needs to be created "with" makeCacheMatrix
+##  after that, the inverse of said matrix should be accessed via cacheSolve
+##  Sample Code:
+##    my_m <- matrix(c(1,2,3,4),nrow=2,ncol=2)
+##    t <- makeCacheMatrix(my_m)
+##    cacheSolve(t)
+##    cacheSolve(t) #this call should display cache data
 
-## Write a short comment describing this function
 
+## Creates a matrix that caches its inverse
+## USAGE: cachedMatrix <- makeCacheMatrix(your_matrix)
+## NOTES: Access the cached inverse via cacheSolve(cachedMatrix)
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -19,8 +27,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse of a makeCacheMatrix using cache or live data as needed
+## USAGE: inversed <- cacheSolve(cachedMatrix)
+## NOTES: Create the cached matrix via makeCacheMatrix(your_matrix)
 cacheSolve <- function(x, ...) {
     m <- x$getinverse()
     if (!is.null(m)) {
@@ -31,5 +40,4 @@ cacheSolve <- function(x, ...) {
     m <- solve(data, ...)
     x$setinverse(m)
     m
-        ## Return a matrix that is the inverse of 'x'
 }
